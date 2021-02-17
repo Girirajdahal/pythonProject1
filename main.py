a6cb3575e2b875a724b14ab45d03cbc78f30630e
prasiddahal <giriraj.dahal16@gmail.com>
2021-02-17 11:06:31 +0545

    perimeter

---
+++ main.py
@@ -0,0 +1,5 @@
+# program to calculate the perimeter of rectangle
+length=int(input('Enter the value of length: '))
+breadth=int(input('Enter the value of breadth: '))
+perimeter=2*(length+breadth)
+print('The perimeter of rectangle is ',perimeter)
